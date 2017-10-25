bd45a9789c053ac4f5428b526116303584090223
vrombouts <val.rom@hotmail.com>
2017-10-25 14:48:06 +0200

    general checker taking Array[Set[Int]]=>Array[Set[Int]] as arguments  for the AllDifferent constraint

--- src/main/scala/Checker.scala
+++ src/main/scala/Checker.scala
@@ -12,24 +12,22 @@ object Checker {
     }yield new Variable(set)
   }
   def Generator_List_Of_Variables(n:Int): Gen[List[Variable]]={
-    Gen.containerOfN[List,Variable](n,Generator_variable())
+    Gen.containerOfN[List,Variable](21,Generator_variable())
   }
 
-  def check_AllDifferent(Body : List[Variable] => Unit): Unit = {
+  /*def check_AllDifferent(Body : List[Variable] => Unit): Unit = {
     val n = 10
     forAll(Generator_List_Of_Variables(n)){x =>
       check_AllDifferent(x,Body)
     }.check
-    
+    //add limit cases
+  }*/
 
-  }
-
-
-  def check_AllDifferent(variables:List[Variable],Body:List[Variable] => Unit): Boolean ={
+  def check_AllDifferent(variables:List[Variable],body:List[Variable] => Unit): Boolean ={
     val result = generate_solutions(variables, AllDifferent)
     println(result)
     println("#################################")
-    Body(variables)
+    body(variables)
     println(variables)
     for (i <- result.indices) {
       if (!result(i).same_domain(variables(i))) {
@@ -38,9 +36,50 @@ object Checker {
     }
     true
   }
+//////////////////////////////////////////////////////////////
 
- def main(args: Array[String]): Unit ={
-   check_AllDifferent(AllDifferent)
+  /*
+   * This function check if the constraint passed in argument apply correctly an
+   * allDifferent constraint with arc consistency.
+   */
+  def check_AllDifferent(constraint:Array[Set[Int]]=>Array[Set[Int]]): Boolean = {
+    forAll(Gen.containerOf[Array,Set[Int]](Generator)){ x =>
+      check_AllDifferent(x,constraint)
+    }.check
+
+    val test1 = Array(Set(0,1,2),Set(0))
+    check_AllDifferent(test1,constraint)
+    val test2 = Array(Set(0,1),Set(0,1),Set(0,1,2))
+    check_AllDifferent(test2,constraint)
+    val test3 = Array(Set(0),Set(1),Set(2))
+    check_AllDifferent(test3,constraint)
+    val test4 = Array(Set(0,1),Set(1,2),Set(2,3),Set(3,4),Set(4,5),Set(5,0))
+    check_AllDifferent(test4,constraint)
+    val test5 = Array(Set(0,1,2))
+    check_AllDifferent(test5,constraint)
+
+  }
+
+  def check_AllDifferent(variables:Array[Set[Int]],constraint_tested:Array[Set[Int]]=>Array[Set[Int]]): Boolean ={
+    //We first compute the domains generated after the application of the constraint.
+    val reduced_domains: Array[Set[Int]] = constraint_tested(variables)
+    // Then we generate the domains that reduced_domains should have
+    val true_reduced_domains: Array[Set[Int]] = ??? //peut-importe(variables,AllDifferent)
+
+    //Finally, we compare the two. If they are not equals, the constraint is not correct.
+    for(i<- reduced_domains.indices){
+      if(!true_reduced_domains(i).equals(reduced_domains(i))){
+        println("failed for: "+ variables)
+        println("you should have: "+ true_reduced_domains)
+        println("but you had "+ reduced_domains)
+        return false
+      }
+    }
+    true
+  }
+
+  def main(args: Array[String]): Unit ={
+    //check_AllDifferent(AllDifferent)
   }
 
 }
