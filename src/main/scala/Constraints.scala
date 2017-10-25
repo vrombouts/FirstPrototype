3534330607817d6442305a8ac910c251dae2b3ff
vrombouts <val.rom@hotmail.com>
2017-10-25 16:23:43 +0200

    cross product with stream implemented

--- src/main/scala/Constraints.scala
+++ src/main/scala/Constraints.scala
@@ -1,8 +1,57 @@
+import scala.collection.immutable.Stream.cons
+
 /**
   * Created by valentin on 13.10.17.
   */
 
 object Constraints {
+
+  def cartesian(sol:Stream[Array[Int]],variable: Set[Int]): Stream[Array[Int]] = {
+    var new_stream : Stream[Array[Int]] = Stream.empty
+    for(s<-sol) {
+      for (value <- variable) {
+        val array = Array.concat(s,Array(value))
+        val strm: Stream[Array[Int]] = cons(array,Stream.empty)
+        new_stream = new_stream.append(strm)
+      }
+    }
+    new_stream
+  }
+
+  def instantiate_stream(variable:Set[Int]): Stream[Array[Int]]={
+    var stream = Stream.empty[Array[Int]]
+    for(i<-variable){
+      stream = stream.append(cons(Array(i),Stream.empty))
+    }
+    stream
+  }
+
+  def cartesian_product(variables:Array[Set[Int]],constraint:Array[Int]=>Boolean) : Array[Set[Int]]={
+    var stream:Stream[Array[Int]] = instantiate_stream(variables(0))
+    for(i<- 1 until variables.length){
+      stream = cartesian(stream,variables(i)).filter(constraint)
+    }
+    val solutions: Array[Array[Int]] = stream.toArray
+    toDomains(solutions)
+  }
+
+  def toDomains(solutions: Array[Array[Int]]): Array[Set[Int]] ={
+    val variables: Array[Set[Int]] = new Array[Set[Int]](solutions(0).length)
+    for(i<-variables.indices){
+      var domain = Set.empty[Int]
+      for(j<-solutions.indices){
+        domain +=  solutions(j)(i)
+      }
+      variables(i) = collection.mutable.SortedSet(domain.toList:_*).toSet
+    }
+    variables
+  }
+
+  def AllDifferent(solution: Array[Int]):Boolean = {
+    val set = solution.toSet
+    if(set.size!=solution.length) return false
+    true
+  }
 
   def AllDifferent(x:List[Variable]):Unit = {
     var change=true
@@ -84,17 +133,19 @@ object Constraints {
       result.sortWith(_ compare_id _)
     }
 
+
+
+
+
   def main(args: Array[String]) {
-    val x = Checker.Generator_List_Of_Variables(10).sample
-    var variables:List[Variable]=null
-    x match {
-      case Some(i) => variables=i
-      case None => variables=null
+    //val x = Checker.Generator_List_Of_Variables(10).sample
+    val variables = Array(Set(0,1,2))
+    val result: Array[Set[Int]] = cartesian_product(variables,AllDifferent)
+    println("result: [")
+    for(i<-result.indices){
+      println(result(i))
     }
-    val result= generate_solutions(variables,AllDifferent)
-    println(result)
-    println("###############")
-    println(variables)
+    print("]")
   }
 
 
