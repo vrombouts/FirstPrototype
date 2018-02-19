663e3cb2b2f0beb56560675ed415904c1753443e
root <valentin.rombouts@student.uclouvain.be>
2018-02-19 15:41:27 +0000

    correction of detactable precedences and not last functions

--- src/main/scala/checker/UnaryResource.scala
+++ src/main/scala/checker/UnaryResource.scala
@@ -24,23 +24,25 @@ class UnaryResource {
   def notLast(activities: Array[Activity]): Array[Activity] = {
     //TODO: check map is used
     //TODO: check sort activities in correct order
+    //look at possible duration of 0
     val ectMap: mutable.Map[Array[Activity], Int] = mutable.Map[Array[Activity],Int]()
     val lctPrime = activities.map(x => x.lct)
-    val sortedActivities = activities.sortWith(_.lst  < _.lst)
+    val sortedActivitiesInLst = activities.sortWith(_.lst  < _.lst)
+    val sortedActivitiesInLct = activities.sortWith(_.lct  < _.lct)
     var indexQueue: Int = 0
     var theta: Array[Activity] = Array()
-    sortedActivities.foreach { activity =>
-      while(activity.lct>sortedActivities(indexQueue).lst){
-        if(activity!=sortedActivities(indexQueue))
-          theta = theta :+ sortedActivities(indexQueue)
+    sortedActivitiesInLct.foreach { activity =>
+      while(indexQueue<activities.length && activity.lct>sortedActivitiesInLst(indexQueue).lst){
+        if(activity!=sortedActivitiesInLst(indexQueue))
+          theta = theta :+ sortedActivitiesInLst(indexQueue)
         indexQueue += 1
       }
       if(theta.nonEmpty && earliestCompletionTime(theta, ectMap)>activity.lst){
-        theta = theta :+ activity
         val index:Int = activities.indexOf(activity)
-        val lst = sortedActivities(indexQueue-1).lst
+        val lst = sortedActivitiesInLst(indexQueue-1).lst
         if(lst < lctPrime(index)) lctPrime(index) = lst
-      }else if(theta.isEmpty) theta = theta :+ activity
+      }
+      theta = theta :+ activity
     }
     for(i <- lctPrime.indices){
       activities(i).lctReduce(lctPrime(i))
@@ -75,35 +77,36 @@ class UnaryResource {
    */
   def arrayToQueue(a:Array[Activity]) : mutable.Queue[Activity] = {
     var q : mutable.Queue[Activity] = mutable.Queue()
-    for(i <- a.indices){
-      q += a(i)
-    }
+    for(i <- a){ q += i}
     q.sortWith(_.lst < _.lst)
-    q
   }
 
-  def detectablePrecedences(activities:Array[Activity]) : Unit={
+  def detectablePrecedences(activities:Array[Activity]) : Array[Activity]={
     val ectMap: mutable.Map[Array[Activity], Int] = mutable.Map[Array[Activity],Int]()
     var theta:Array[Activity] = Array()
     val queue:mutable.Queue[Activity] = arrayToQueue(activities)
     var estUpdated:Array[Int] = Array()
     val sortedActivities=activities.sortWith(_.ect< _.ect)
     sortedActivities.foreach{ activity =>
+      var addedItself = false
       while(queue.nonEmpty && activity.ect > queue.front.lst){
         val a:Activity = queue.dequeue()
         if(a != activity)
           theta=theta :+ a
+        else
+          addedItself = true
       }
-      if(activity.est < earliestCompletionTime(theta,ectMap))
+      if(theta.nonEmpty && activity.est < earliestCompletionTime(theta,ectMap))
         estUpdated = estUpdated :+ earliestCompletionTime(theta,ectMap)
       else
         estUpdated = estUpdated :+ activity.est
+      if(addedItself) theta = theta :+ activity
     }
 
     for(i <- sortedActivities.indices){
       val index:Int = activities.indexOf(sortedActivities(i))
       activities(index).estReduce(estUpdated(i))
     }
-
+    activities
   }
 }
