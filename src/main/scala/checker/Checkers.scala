f50b72503662b8cef46bda1e44961002549ebcd3
root <valentin.rombouts@student.uclouvain.be>
2018-05-03 16:20:11 +0000

    Checkers usable in java

--- src/main/scala/checker/Checkers.scala
+++ src/main/scala/checker/Checkers.scala
@@ -1,11 +1,15 @@
 package checker
 
+import Conversions._
+
 object Checkers {
   def trueConstraint(solution: Array[Int]): Boolean = true
 
   def falseConstraint(solution: Array[Int]): Boolean = false
 
   def allDifferent(solution: Array[Int]): Boolean = solution.toSet.size == solution.length
+
+  def allDifferent(): Array[Int] => Boolean = x => allDifferent(x)
 
   def element(solution: Array[Int]): Boolean = {
     if (solution.length <= 2) return false
@@ -15,10 +19,14 @@ object Checkers {
     element(X, i, v)
   }
 
+  def element(): Array[Int] => Boolean = x => element(x)
+
   def element(solution: Array[Int], i: Int, v: Int): Boolean = {
     if (i < 0 || i >= solution.length) return false
     solution(i) == v
   }
+
+  def element(i: Int, v: Int): Array[Int] => Boolean = x => element(x, i, v)
 
   def sum(solution: Array[Int], constant: Int = 0, operator: String = "="): Boolean = {
     if (operator.equals("=")) return solution.sum == constant
@@ -30,11 +38,18 @@ object Checkers {
     false
   }
 
+  def sum(constant: Int, operator: String = "="): Array[Int] => Boolean = x => sum(x, constant, operator)
+
   def table(solution: Array[Int], table: Set[Array[Int]]): Boolean = {
     table.exists(x => x.sameElements(solution))
   }
 
-  def gcc(solution: Array[Int], values: Array[Int]): Boolean = {
+  def table(t: Set[Array[Int]]): Array[Int] => Boolean = x => table(x, t)
+
+  def table(t: java.util.Set[Array[Integer]]): Array[Int] => Boolean =
+    x => table(x, t)
+
+  def gccVar(solution: Array[Int], values: Array[Int]): Boolean = {
     if (solution.length < values.length) return false
     var valuesCount: Map[Int, Int] = Map()
     val variables = solution.dropRight(values.length)
@@ -51,6 +66,8 @@ object Checkers {
     true
   }
 
+  def gccVar(values: Array[Int]): Array[Int] => Boolean = x => gccVar(x, values)
+
   def gcc(solution: Array[Int], occurences: Array[Int], values: Array[Int]): Boolean = {
     assert(occurences.length == values.length)
     if (solution.length < occurences.sum) return false
@@ -66,4 +83,6 @@ object Checkers {
     }
     true
   }
+
+  def gcc(occurences: Array[Int], values: Array[Int]): Array[Int] => Boolean = x => gcc(x, occurences, values)
 }
