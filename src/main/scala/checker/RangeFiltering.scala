e54ee8f953dbd716b509a151ce50eb4929863f10
root <valentin.rombouts@student.uclouvain.be>
2018-05-09 10:48:57 +0000

    unti tests for the range filtering (static and incremental)

--- src/main/scala/checker/RangeFiltering.scala
+++ src/main/scala/checker/RangeFiltering.scala
@@ -1,23 +1,32 @@
 package checker
 
-class RangeFiltering(checker : Array[Int] => Boolean) extends Filter {
+import java.util.function.Function
+
+import Conversions.checkerToScalaFunction
+
+class RangeFiltering(checker: Array[Int] => Boolean) extends Filter {
+
+  def this(jChecker: Function[Array[Integer], java.lang.Boolean]) = this(checkerToScalaFunction(jChecker))
+
   override def filter(variables: Array[Set[Int]]): Array[Set[Int]] = {
-    val intervals: Array[Interval] = variables.map{new Interval(_)}
-    while(range(intervals)){}
+    val intervals: Array[Interval] = variables.map(x => if (x.nonEmpty) new Interval(x) else throw new NoSolutionException)
+    filterIntervals(intervals)
     intervals.map(x => x.domain)
   }
 
-  def range(intervals: Array[Interval]): Boolean = {
-    intervals.indices.foldLeft(false){(Acc, x) => if(range(x, intervals)) true else Acc}
+  private[this] def filterIntervals(intervals: Array[Interval]): Unit = {
+    if (intervals.indices.foldLeft(false) { (acc, x) => if (filterIndex(x, intervals)) true else acc })
+      filterIntervals(intervals)
   }
 
-  private[this] def range(index: Int, intervals: Array[Interval]): Boolean = {
+  private[this] def filterIndex(index: Int, intervals: Array[Interval]): Boolean = {
     val domain: Set[Int] = intervals(index).domain
     intervals(index).dom = domain.filter(findASolution(index, intervals, _))
-    domain.size!=intervals(index).dom.size
+    if (intervals(index).dom.isEmpty) throw NoSolutionException()
+    domain.size != intervals(index).dom.size
   }
 
-  private[this] def findASolution(index : Int, intervals: Array[Interval], value:Int): Boolean = {
+  private[this] def findASolution(index: Int, intervals: Array[Interval], value: Int): Boolean = {
     val currentSol: Array[Int] = Array.fill(intervals.length)(0)
     currentSol(index) = value
 
---
+++ src/test/scala/checker/RangeFilteringTests.scala
@@ -0,0 +1,105 @@
+package checker
+
+import org.scalatest.FlatSpec
+
+class RangeFilteringTests extends FlatSpec {
+  val rangeTrue = new RangeFiltering(_ => true)
+  val rangeAllDiff = new RangeFiltering(Checkers.allDifferent())
+
+  "Calling filter for trueConstraint" should "return the input domains except if there is an empty domain" in {
+    var a: Array[Set[Int]] = rangeTrue.filter(Array(Set(0), Set(1)))
+    var b: Array[Set[Int]] = Array(Set(0), Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+    a = rangeTrue.filter(Array(Set(1), Set(1)))
+    b = Array(Set(1), Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+    a = rangeTrue.filter(Array(Set(0, 1), Set(0, 1)))
+    b = Array(Set(0, 1), Set(0, 1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+    assertThrows[NoSolutionException] {
+      rangeTrue.filter(Array(Set(), Set(1)))
+    }
+  }
+
+  "Calling filter for AllDifferent on domains [1] [1]" should "return an exception" in {
+    assertThrows[NoSolutionException] {
+      rangeAllDiff.filter(Array(Set(1), Set(1)))
+    }
+  }
+
+  "Calling filter for AllDifferent on domains [0,1] [0,1]" should "return domains [0,1] [0,1]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 1), Set(0, 1)))
+    val b: Array[Set[Int]] = Array(Set(0, 1), Set(0, 1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [0,1] [0,1] [0,1]" should "return an exception" in {
+    assertThrows[NoSolutionException] {
+      rangeAllDiff.filter(Array(Set(0, 1), Set(0, 1), Set(0, 1)))
+    }
+  }
+
+
+  "Calling filter for AllDifferent on domains [0,1] [1]" should "return domains [0] [1]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 1), Set(1)))
+    val b: Array[Set[Int]] = Array(Set(0), Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [0,1,2] [2,3] [1]" should "return domains [0,2] [2,3] [1]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 1, 2), Set(2, 3), Set(1)))
+    val b: Array[Set[Int]] = Array(Set(0, 2), Set(2, 3), Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [0,1,2] [1,2] [2,3] [0]" should "return domains [1,2] [1,2] [3] [0]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 1, 2), Set(1, 2), Set(2, 3), Set(0)))
+    val b: Array[Set[Int]] = Array(Set(1, 2), Set(1, 2), Set(3), Set(0))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [1,2,3] [0,5] [1,5,6] [1,2] [2,3] [0]" should "return domains [1,2,3] [5] [6] [1,2] [2,3] [0]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(1, 2, 3), Set(0, 5), Set(1, 5, 6), Set(1, 2), Set(2, 3), Set(0)))
+    val b: Array[Set[Int]] = Array(Set(1, 2, 3), Set(5), Set(6), Set(1, 2), Set(2, 3), Set(0))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [0,3,4] [1,3], [4], [1]" should "return domains [0] [3] [4] [1] " in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 3, 4), Set(1, 3), Set(4), Set(1)))
+    val b: Array[Set[Int]] = Array(Set(0), Set(3), Set(4), Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "Calling filter for AllDifferent on domains [0,3,6,8,10] [0,1] [10] [1] [6]" should "return domains [3,8] [0] [10] [1] [6] " in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(0, 3, 6, 8, 10), Set(0, 1), Set(10), Set(1), Set(6)))
+    val b: Array[Set[Int]] = Array(Set(3, 8), Set(0), Set(10), Set(1), Set(6))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "calling filter for AllDifferent on domain [1]" should "return [1]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(1)))
+    val b: Array[Set[Int]] = Array(Set(1))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "calling filter for AllDifferent on domain [1,4,6][1,4,6][1,4,6][1,4,6]" should "return [1,4,6][1,4,6][1,4,6][1,4,6]" in {
+    val a: Array[Set[Int]] = rangeAllDiff.filter(Array(Set(1, 4, 6), Set(1, 4, 6), Set(1, 4, 6), Set(1, 4, 6)))
+    val b: Array[Set[Int]] = Array(Set(1, 4, 6), Set(1, 4, 6), Set(1, 4, 6), Set(1, 4, 6))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+  "calling filter for false constraint on domain [1,2,3]" should "throw a noSolutionException" in {
+    val rangeFalse = new RangeFiltering(Checkers.falseConstraint)
+    assertThrows[NoSolutionException] {
+      rangeFalse.filter(Array(Set(1, 2, 3)))
+    }
+  }
+
+  "calling filter" should "use its checker only on complete solutions" in {
+    val C = new RangeFiltering(x => x.length == 4)
+    val a: Array[Set[Int]] = C.filter(Array(Set(1, 2), Set(1, 2), Set(1, 2), Set(1, 2)))
+    val b: Array[Set[Int]] = Array(Set(1, 2), Set(1, 2), Set(1, 2), Set(1, 2))
+    assert((a zip b).forall(x => x._1.equals(x._2)))
+  }
+
+}
