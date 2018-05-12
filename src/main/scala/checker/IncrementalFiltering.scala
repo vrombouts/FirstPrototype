a8f01a45456835ddf2ddc4d9c86fe441b307229f
root <valentin.rombouts@student.uclouvain.be>
2018-05-12 14:14:09 +0000

    deprecated stack changed as List

--- src/main/scala/checker/IncrementalFiltering.scala
+++ src/main/scala/checker/IncrementalFiltering.scala
@@ -2,15 +2,13 @@ package checker
 
 import checker.incremental.{BranchOp, Pop, Push, RestrictDomain}
 
-import scala.collection.mutable
-
-//to do new IncrementalFiltering(myStaticFilter) to have a filterWithState from a static filter.
+//to do new IncrementalFiltering(myStaticFilter) and have a filterWithState from a static filter.
 class IncrementalFiltering(filter: Filter) extends FilterWithState {
   //use list instead of stack.
-  private[this] var domainsStorage: mutable.Stack[Array[Set[Int]]] = _
+  private[this] var trailStack : List[Array[Set[Int]]] = _
 
   override def setup(variables: Array[Set[Int]]): Array[Set[Int]] = {
-    domainsStorage = mutable.Stack()
+    trailStack = List()
     filter.filter(variables)
   }
 
@@ -27,14 +25,16 @@ class IncrementalFiltering(filter: Filter) extends FilterWithState {
   }
 
   def push(currentDomain: Array[Set[Int]]): Array[Set[Int]] = {
-    domainsStorage.push(currentDomain)
+    trailStack = currentDomain :: trailStack
     currentDomain
   }
 
   def pop(currentDomain: Array[Set[Int]]): Array[Set[Int]] = {
-    if (domainsStorage.nonEmpty)
-      domainsStorage.pop()
-    else
+    if(trailStack.nonEmpty){
+      val vars = trailStack.head
+      trailStack = trailStack.tail
+      vars
+    }else
       currentDomain
   }
 }
